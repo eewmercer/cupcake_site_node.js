7bdf94da136016640a61e5288b3821b80ef64a9d
Emmaline Mercer <eewmercer@gmail.com>
2025-03-13 17:57:52 -0400

    Add middleware

--- routes/route.js
+++ routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 const errorController = require('../controllers/error');
 const cupcakesController = require('../controllers/cupcakes');
+const isLoggedIn = require('../middleware/isLoggedIn');
 
 router.get('/', adminController.getLoginPage)
 router.post('/user-login', adminController.postLoginPage)
@@ -10,17 +11,17 @@ router.post('/user-login', adminController.postLoginPage)
 router.get('/register', adminController.getRegisterPage)
 router.post('/register', adminController.postRegisterPage)
 
-router.get('/system', adminController.getAddCupcakePage)
+router.get('/system', isLoggedIn, adminController.getAddCupcakePage)
 
-router.get('/about', adminController.getAboutPage)
+router.get('/about', isLoggedIn, adminController.getAboutPage)
 
-router.post('/add-cupcake', cupcakesController.postAddCupcake)
+router.post('/add-cupcake', isLoggedIn, cupcakesController.postAddCupcake)
 
-router.get('/edit-cupcake', cupcakesController.getEditCupcake)
+router.get('/edit-cupcake', isLoggedIn, cupcakesController.getEditCupcake)
 
-router.post('/complete-edit', cupcakesController.postEditCupcake)
+router.post('/complete-edit', isLoggedIn, cupcakesController.postEditCupcake)
 
-router.post('/delete-cupcake', cupcakesController.postDeleteCupcake)
+router.post('/delete-cupcake', isLoggedIn, cupcakesController.postDeleteCupcake)
 
 //404 => ERROR
 router.get('/404', errorController.get404)
